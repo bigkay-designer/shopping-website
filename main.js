00a75d52e04996fe9dea0ecf959f17fb1b907154
kay <ibrahimkhalid478@gmail.com>
2020-01-30 05:26:30 +0000

    updated 30/01 - 05:26

--- main.js
+++ main.js
@@ -17,44 +17,56 @@ let itemQuanArr = Array.prototype.slice.call(itemQuan);
 let itemPriceArr = Array.prototype.slice.call(itemPrice);
 let itemDescArr = Array.prototype.slice.call(itemDesc);
 
-// looping through btns
+cartBtn.forEach(function(btn) {
+  btn.addEventListener('click', function(event) {
+    // for (i = 0; i < btn.length; i++) {
+    //   console.log(btn[i]);
+    // }
+    let addInfo1 =
+      event.target.parentElement.children[1].children[0].textContent;
+    let addInfo2 = event.target.parentElement.children[1].children[1].value;
+    let addInfo3 =
+      event.target.parentElement.children[1].children[2].textContent;
+    addInfo3 = parseInt(addInfo3);
+    let poundSign = (document.querySelectorAll('.item-price span').textContent =
+      '£');
+    let total = addInfo3 * addInfo2;
+    addInfo3 = poundSign + addInfo3;
+    total = poundSign + total;
 
-for (i = 0; i < cartBtn.length; i++) {
-  cartBtn[i].addEventListener('click', function() {
-    addToCart();
+    // console.log(addInfo3);
+
+    // startinng  the table
+    let tableArr = ['Products', 'Quantity', 'Price', ' Total'];
+    let tableData = [addInfo1, addInfo2, addInfo3, total];
+    //creating a header for the table
+    let perHeader = 4,
+      html = '<table><tr>';
+
+    for (i = 0; i < tableArr.length; i++) {
+      html += '<th>' + tableArr[i] + '</th>';
+      let nextHead = i + 1;
+      if (nextHead % perHeader == 0 && nextHead != tableArr.length) {
+        html += '</tr><tr>';
+      }
+    }
+
+    //adding the data to the table
+    let perRow = 4;
+    html += '<tr>';
+
+    for (r = 0; r < tableData.length; r++) {
+      html += '<td>' + tableData[r] + '</td>';
+      let nextRow = r + 1;
+      if (nextRow % perRow == 0 && nextRow != tableData.length) {
+        html += '</tr><tr>';
+      }
+    }
+
+    console.log(html);
+    document.getElementById('cart-info').innerHTML = html;
   });
-}
-
-function addToCart() {
-  let tableArr = ['Products', 'Quantity', 'Price', ' Total'];
-  let tableData = [t1.textContent, '12'];
-  //creating a header for the table
-  let perHeader = 4,
-    html = '<table><tr>';
-
-  for (i = 0; i < tableArr.length; i++) {
-    html += '<th>' + tableArr[i] + '</th>';
-    let nextHead = i + 1;
-    if (nextHead % perHeader == 0 && nextHead != tableArr.length) {
-      html += '</tr><tr>';
-    }
-  }
-
-  //adding the data to the table
-  let perRow = 4;
-  html += '<tr>';
-
-  for (r = 0; r < tableData.length; r++) {
-    html += '<td>' + tableData[r] + '</td>';
-    let nextRow = r + 1;
-    if (nextRow % perRow == 0 && nextRow != tableData.length) {
-      html += '</tr><tr>';
-    }
-  }
-
-  console.log(html);
-  document.getElementById('cart-info').innerHTML = html;
-}
+});
 
 cartImg.addEventListener('click', function() {
   $('#cart-info').toggle();
