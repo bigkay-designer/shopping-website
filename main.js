5d5f3cd16444938c5def3b723154e227b180dfa5
kay <ibrahimkhalid478@gmail.com>
2020-02-02 05:22:22 +0000

    latest

--- main.js
+++ main.js
@@ -3,76 +3,95 @@ let cartImg = document.querySelector('#cart-img');
 // let cartRow = document.querySelector('#cart-row');
 // let cartRowData = document.querySelector('#cartrow-data');
 // let tableBody = document.querySelector('tbody');
-let t1 = document.querySelector('#t1');
-let t2 = document.querySelector('#t2');
-let t3 = document.querySelector('t3');
-let t4 = document.querySelector('t4');
 let itemDesc = document.querySelectorAll('.item-desc');
 let itemQuan = document.querySelectorAll('#table-input');
 let itemPrice = document.querySelectorAll('.item-price');
 let cartBtn = document.querySelectorAll('#cart-btn');
 
+let log = console.log;
+
 //changing it an array
-let itemQuanArr = Array.prototype.slice.call(itemQuan);
-let itemPriceArr = Array.prototype.slice.call(itemPrice);
-let itemDescArr = Array.prototype.slice.call(itemDesc);
+// let itemQuanArr = Array.prototype.slice.call(itemQuan);
+// let itemPriceArr = Array.prototype.slice.call(itemPrice);
+// let itemDescArr = Array.prototype.slice.call(itemDesc);
 
 cartBtn.forEach(function(btn) {
   btn.addEventListener('click', function(event) {
     updateCart();
+    
   });
 });
 
 function updateCart() {
+
+  for (i = 0; i < itemQuan.length; i++) {
+    let quanResult = itemQuan[i]
+    if (isNaN(quanResult.value) || quanResult.value <= 0) {
+      itemQuan.value = 1
+    }
+  }
   let addInfo1 = event.target.parentElement.children[1].children[0].textContent;
-  let addInfo2 = event.target.parentElement.children[1].children[1].value;
-  let addInfo3 = event.target.parentElement.children[1].children[2].textContent;
-  addInfo3 = parseInt(addInfo3);
-  let poundSign = (document.querySelectorAll('.item-price span').textContent =
-    '£');
-  let total = addInfo3 * addInfo2;
-  addInfo3 = poundSign + addInfo3;
-  total = poundSign + total;
+  // let addInfo2 = event.target.parentElement.children[1].children[1].value ;
+  // let addInfo3 = event.target.parentElement.children[1].children[2].textContent;
+  // addInfo3 = parseInt(addInfo3);
+  // let poundSign = (document.querySelectorAll('.item-price span').textContent =
+  // '£');
+  // subTotal = addInfo3 * addInfo2
+  // addInfo3 = poundSign + addInfo3;
+  // total = poundSign + total;
+  
+  //final price code
+  let tableData = []
 
-  // console.log(addInfo3);
-
+  let items = function (product, quantity, price, total) {
+    this.product = addInfo1
+    this.quantity = updatePrice()
+    this.price = updatePrice()
+    this.total = finalPrice
+  }
+  
+  tableData.push(new items)
   // startinng  the table
-  let tableArr = ['Products', 'Quantity', 'Price', ' Total'];
-  let tableData = [addInfo1, addInfo2, addInfo3, total];
-  //creating a header for the table
-  let perHeader = 4,
-    html = '<table><tr>';
-
-  for (i = 0; i < tableArr.length; i++) {
-    html += '<th>' + tableArr[i] + '</th>';
-    let nextHead = i + 1;
-    if (nextHead % perHeader == 0 && nextHead != tableArr.length) {
-      html += '</tr><tr>';
-    }
-  }
-
+  let tbl = document.querySelector('#cart-table')
+  let row = tbl.insertRow()
   //adding the data to the table
-  let perRow = 4;
-  html += '<tr>';
-
-  for (r = 0; r < tableData.length; r++) {
-    html += '<td>' + tableData[r] + '</td>';
-    let nextRow = r + 1;
-    if (nextRow % perRow == 0 && nextRow != tableData.length) {
-      html += '</tr><tr>';
-    }
+  for (let user of tableData) {
+    let cartList = row.innerHTML =
+      '<td>' + user.product + '</td>' +
+      '<td>' + user.quantity + '</td>' +
+      '<td>' + '£' + user.price + '</td>' +
+      '<td>' + user.total + '</td>';
+    // console.log(cartList)
+    // log()
   }
-  console.log(html);
-  document.getElementById('cart-info').innerHTML = html;
+
 }
+
 
 cartImg.addEventListener('click', function() {
   $('#cart-info').toggle();
   $('#cart-table').css('background-color', '#adcee9');
 });
 
+
+function updatePrice() {
+  let cartContainer = document.getElementsByClassName('item-paras')
+  finalPrice = 0
+  for (i = 0; i < cartContainer.length; i++){
+    let cartRow = cartContainer[i]
+    let priceELement = cartRow.getElementsByClassName('item-price')[0]
+    let quantityElement = cartRow.getElementsByClassName('table-input')[0]
+
+    let price = parseInt(priceELement.innerText)
+    let quantity = quantityElement.value
+    finalPrice = finalPrice + (price * quantity)
+    log(finalPrice)
+  }
+}
+// updatePrice()
 // for (i = 0; i < itemPriceArr.length; i++) {
 //     let itemPriceTxt = itemPriceArr[i].innerHTML;
 //     let itemQuanTxt = itemQuanArr[i].value;
 //     let itemDescTxt = itemDescArr[i].innerHTML;
 //   }
+
